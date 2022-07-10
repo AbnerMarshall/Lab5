35625471ad1062866f5d34640e9181c4575233c0
Abner Marshall <kold.oktober@gmail.com>
2022-07-09 18:42:04 -0600

    Added Image Element to Main-header and made some css changes to round corners and add border.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="Main-App">
       <div className="Main-Layout">
-        <div className="Main-header">Header</div>
+        <div className="Main-header"></div>
         <div className="Main-content">Content</div>
         <div className="Main-footer">Footer</div>
     </div>
